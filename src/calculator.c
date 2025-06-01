2a4cdacd4ddc4a3d24c1b5fcb1c93ce5487cc471
Sulaymon <basharovsuleyman1403@gmail.com>
2025-06-01 14:18:40 +0500

    fine

--- src/calculator.c
+++ src/calculator.c
@@ -16,5 +16,22 @@ int main(){
     int hate_polyglot = simple - navi;
     printf("%d - %d = %i\n", simple, navi, hate_polyglot);
 
+
+
+    if (pose%2 == 0) 
+    { printf ("aaaaaaaaaaaa\n");
+    } else {
+      printf("1111111111"); }
+    if (iro%2 == 0) 
+    { printf ("bbbbbbbbbbbb\n");
+    } else {
+      printf("22222222222222"); }
+
+    if (hate_polyglot%2 == 0) 
+    { printf ("ccccccccccc\n");
+    } else {
+      printf("3333333333"); }
+
+
    return 0;
 }
