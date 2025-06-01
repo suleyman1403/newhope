98f4cfcc5527aaddbb37df0c1556c5c5ef06aeea
Sulaymon <basharovsuleyman1403@gmail.com>
2025-06-01 13:34:49 +0500

    calculatoor

---
+++ src/calculator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    
+    int simple = 69;
+    int navi = 108;
+
+    int pose = simple + navi;
+    printf("%d + %d = %i\n", simple, navi, pose);
+    int iro = simple * navi;
+    printf("%d * %d = %d\n", simple, navi, iro);
+    float fuck = (float) navi / simple;
+    printf("%d / %d = %f\n",  simple, navi, fuck);
+    int hate_polyglot = simple - navi;
+    printf("%d - %d = %i\n", simple, navi, hate_polyglot);
+
+   return 0;
+}
